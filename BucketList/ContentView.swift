f12794b69f5e35793cc891997a6eff940da0df73
Mohammad Aazam <tech.azam@gmail.com>
2022-07-11 19:51:28 +0530

    f1- Add Map with centre point.

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,12 +5,22 @@
 //  Created by QBUser on 09/07/22.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50))
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Map(coordinateRegion: $mapRegion)
+                .ignoresSafeArea()
+
+            Circle()
+                .foregroundColor(.blue.opacity(0.3))
+                .frame(width: 44, height: 44)
+        }
     }
 }
 
